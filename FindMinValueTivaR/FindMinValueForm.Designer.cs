9a460adf8cc0c4ce0b084af94b7c5e697ecbf51c
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-06-04 11:13:31 -0400

    Initial commit

---
+++ FindMinValueTivaR/FindMinValueForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FindMinValueTivaR
+{
+    partial class FindMinValueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumberSet = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNumberSet
+            // 
+            this.lstNumberSet.FormattingEnabled = true;
+            this.lstNumberSet.Location = new System.Drawing.Point(43, 55);
+            this.lstNumberSet.Name = "lstNumberSet";
+            this.lstNumberSet.Size = new System.Drawing.Size(178, 147);
+            this.lstNumberSet.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(97, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMinValue
+            // 
+            this.lblMinValue.AutoSize = true;
+            this.lblMinValue.Location = new System.Drawing.Point(44, 222);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(111, 13);
+            this.lblMinValue.TabIndex = 2;
+            this.lblMinValue.Text = "The minimum value is:";
+            // 
+            // FindMinValueForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 263);
+            this.Controls.Add(this.lblMinValue);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstNumberSet);
+            this.Name = "FindMinValueForm";
+            this.Text = "Find Min Value By Tiva Rait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumberSet;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMinValue;
+    }
+}
+
---
+++ FindMinValueTivaR/FindMinValueForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 23-05-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Find Min Value
+ * This program randomly generates 10 numbers then adds them to an array. 
+ * The function GetMinValue then compares all of the number in the array to find the smallest one.
+*/
+
+namespace FindMinValueTivaR
+{
+    public partial class FindMinValueForm : Form
+    {
+        public FindMinValueForm()
+        {
+            InitializeComponent();
+        }
+
+        // Function: GetMinValue
+        // Input: int[] tmpArrayOfIntegers - assume all values in the array are smaller than than 501
+        // Output: the min value in the array 
+        // Description: This function accepts an array of integers,...
+        // ... scans the array and returns the minimum value in the array.
+        private int GetMinValue(int[] tmpArrayOfIntegers)
+        {
+            // Variables
+            int tmpMinValue = 500;
+            const int MAX_ARRAY_SIZE = 10;
+
+            // To cycle through array until it is all compared
+            foreach ([] in tmpArrayOfIntegers)
+            /*
+            for (int counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                if (tmpMinValue > tmpArrayOfIntegers[counter])
+                {
+                    // Set the max value to be equal to the current number
+                    tmpMinValue = tmpArrayOfIntegers[counter];
+                }
+            }
+            */
+
+            return tmpMinValue;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables and constants 
+            const int MAX_ARRAY_SIZE = 10;
+            const int MAX_RANDOM_NUMBER = 500;
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+            int counter, randomNumber, minValue;
+            Random randomNumberGenerator = new Random();
+
+            // Clear list box
+            this.lstNumberSet.Items.Clear();
+
+            // To generate the numbers
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                // generate a new random number between 1 and the MAX_ARRAY_SIZE
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                // Assign the random number to the cell at position "counter" in the array
+                arrayOfIntegers[counter] = randomNumber;
+
+                // add the random number to the list 
+                this.lstNumberSet.Items.Add(randomNumber);
+
+                // Refresh the form to display the new item in the list box
+                this.Refresh();
+
+                // To get the Max Value in the array
+                minValue = GetMinValue(arrayOfIntegers);
+
+                // Display the max value in the label
+                this.lblMinValue.Text = "The minimum value is: " + minValue;
+            }
+        }
+    }
+}
